1067d90d51303a61a9d8b7f81041d7002528ebe9
Massimiliano Sartoretto <massimilianosartoretto@gmail.com>
2015-08-23 22:39:55 -0700

    Add ngPhotoswipeProvider

--- angular-photoswipe.js
+++ angular-photoswipe.js
@@ -15,6 +15,7 @@
     return angular
       .module('ngPhotoswipe', [])
       .directive('ngPhotoswipe', ['ngPhotoswipe', ngPhotoswipeDirective])
+      .provider('ngPhotoswipe', ngPhotoswipeProvider);
 
     function ngPhotoswipeDirective() {
       return {
@@ -22,6 +23,47 @@
         scope: {},
         link: function (scope, element, attrs) {}
       }
+    }
+
+    function ngPhotoswipeProvider() {
+      var defaults = {};
+
+      var requiredKeys = [];
+
+      var config;
+
+      this.configure = function(params) {
+        // Check if it is an `object`
+        if (!(params instanceof Object)) {
+          throw new TypeError('Invalid argument: `config` must be an `Object`.');
+        }
+
+        // Extend default configuration
+        config = angular.extend({}, defaults, params);
+
+        // Check if all required keys are set
+        angular.forEach(requiredKeys, function(key) {
+          if (!config[key]) {
+            throw new Error('Missing parameter:', key);
+          }
+        });
+
+        return config;
+      };
+
+      this.$get = function() {
+        if(!config) {
+          throw new Error('ngPhotoswipe must be configured first.');
+        }
+
+        var getConfig = (function() {
+          return config;
+        })();
+
+        return {
+          configuration: getConfig
+        };
+      };
     }
   }
 
