f28a4cf4b03bc25c60feedaea43140ebbf5a2943
Massimiliano Sartoretto <massimilianosartoretto@gmail.com>
2015-08-27 23:10:30 -0700

    Better style

--- angular-photoswipe.js
+++ angular-photoswipe.js
@@ -1,5 +1,5 @@
 /*
-	angular-photoswipe v0.0.0
+	angular-photoswipe v0.0.2
 	(c) 2015 Massimiliano Sartoretto <massimilianosartoretto@gmail.com>
 	License: MIT
 */
@@ -14,10 +14,11 @@
 
     return angular
       .module('ngPhotoswipe', [])
-      .directive('ngPhotoswipe', ['ngPhotoswipe','$compile','$http','$templateCache', ngPhotoswipeDirective])
-      .provider('ngPhotoswipe', ngPhotoswipeProvider);
+      .directive('ngPhotoswipe', ngPhotoswipeDirective);
 
-    function ngPhotoswipeDirective(ngPhotoswipe, $compile, $http, $templateCache) {
+    ngPhotoswipeDirective.$inject = ['$compile','$http','$templateCache'];
+
+    function ngPhotoswipeDirective($compile, $http, $templateCache) {
       return {
         restrict: 'AE',
         replace: true,
@@ -25,68 +26,24 @@
           slides: '=',
           options: '='
         },
-        compile: function ($elm, $attrs) {
-          return {
-            pre: function ($scope, $elm, $attrs, controllers) {
-              $http
-                .get('../../ng-photoswipe.html', { cache: $templateCache })
-                .success(function(html) {
-                  var template = angular.element(html);
-                  var newElm = $compile(template)($scope);
-                  $elm.append(newElm);
-                });
-            },
-            post: function ($scope, $elm, $attrs, controllers) {
-              console.log($scope.uiClass);
-              $scope.start = function(){
-                var pswpElement = document.querySelectorAll('.pswp')[0];
-                console.log(pswpElement);
-                console.log($scope.slides);
-                var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default || false, $scope.slides, $scope.options);
-                gallery.init();
-              };
-            }
-          };
-        }
+        link: linkFn,
+        controllerAs: 'vm',
+        bindToController: true
       };
     }
 
-    function ngPhotoswipeProvider() {
-      var defaults = {};
-
-      var requiredKeys = [];
-
-      var config;
-
-      /* jshint validthis: true */
-      this.configure = function(params) {
-        // Check if it is an `object`
-        if (!(params instanceof Object)) {
-          throw new TypeError('Invalid argument: `config` must be an `Object`.');
-        }
-
-        // Extend default configuration
-        config = angular.extend({}, defaults, params);
-
-        // Check if all required keys are set
-        angular.forEach(requiredKeys, function(key) {
-          if (!config[key]) {
-            throw new Error('Missing parameter:', key);
-          }
+    function linkFn(scope, iElement, iAttrs, vm) {
+      $http
+        .get('../../ng-photoswipe.html', { cache: $templateCache })
+        .success(function(html) {
+          var template = angular.element(html);
+          iElement.append($compile(template)(scope));
         });
 
-        return config;
-      };
-
-      /* jshint validthis: true */
-      this.$get = function() {
-        var getConfig = (function() {
-          return config;
-        })();
-
-        return {
-          configuration: getConfig
-        };
+      scope.start = function(){
+        var pswpElement = document.querySelectorAll('.pswp')[0];
+        var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default || false, scope.slides, scope.options);
+        gallery.init();
       };
     }
   }
