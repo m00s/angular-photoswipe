cac513fb00bf885dd65eb957f9a1506ddc7003fb
Massimiliano Sartoretto <massimilianosartoretto@gmail.com>
2015-12-18 21:35:02 +0100

    Add moar grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,22 +14,61 @@ module.exports = function(grunt) {
           sourceMap: true
         },
         files: {
-          'angular-photoswipe.min.js': ['angular-photoswipe.js']
+          'angular-photoswipe.min.js': [
+            'angular-photoswipe.annotated.js',
+            'templates.js'
+          ]
         }
       }
     },
-    karma: {
-			unit: {
-				configFile: 'karma.conf.js',
-				singleRun: true
-			}
-		},
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['a.js', 'templates.js'],
+        dest: 'aabb.js',
+      },
+    },
+    ngAnnotate: {
+      options: {
+          singleQuotes: true,
+          remove: true,
+          singleQuotes: true
+      },
+      dist: {
+          files: {
+              'angular-photoswipe.annotated.js': ['angular-photoswipe.js']
+          },
+      }
+    },
     jshint: {
       src: ['angular-photoswipe.js', 'tests.js']
+    },
+    clean: {
+      release: ['angular-photoswipe.annotated.js','templates.js']
+    },
+    ngtemplates:  {
+      app:  {
+        src:  'views/**.html',
+        dest: 'templates.js',
+        options:  {
+          htmlmin:  {
+            collapseBooleanAttributes:      true,
+            collapseWhitespace:             true,
+            removeAttributeQuotes:          true,
+            removeEmptyAttributes:          true,
+            removeRedundantAttributes:      true,
+            removeScriptTypeAttributes:     true,
+            removeStyleLinkTypeAttributes:  true
+          },
+          module: 'ngPhotoswipe'
+        }
+      }
     }
   });
 
   grunt.registerTask('test', ['jshint','karma']);
   grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('build', ['uglify', 'jshint']);
+  grunt.registerTask('build', ['ngtemplates','ngAnnotate','uglify','clean']);
 };
