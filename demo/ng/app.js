2f3f08558f128968b135e1bf41363aec484206e1
Massimiliano Sartoretto <massimilianosartoretto@gmail.com>
2015-12-27 12:00:00 +0100

    Update demo

--- demo/ng/app.js
+++ demo/ng/app.js
@@ -2,22 +2,49 @@
 
 angular
   .module('demoApp', ['ngPhotoswipe'])
-  .controller('demoCtrl', function ($scope) {
-    $scope.title = 'ngPhotoswipe';
+  .controller('demoCtrl', demoCtrlFn);
 
-    $scope.slides = [{
-        src: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSrTgaP_TaPuS035ynJDnQbYWJ1RO_bgDHEdImWo25MCuO-58-9Bg',
-        w: 360, h: 360
-      }, {
-        src: 'http://itsadeliverything.com/wordpress/images//Dont-Panic.jpg',
-        w: 1024, h: 1024
-      }
-    ];
+function demoCtrlFn($scope) {
+  var vm = this;
 
-    $scope.opts = {
-      index: 0
-    };
-  });
+  vm.title = 'ngPhotoswipe';
+  vm.startEvent = 'START_GALLERY';
+
+  vm.opts = {
+    index: 0
+  };
+
+  vm.slides = [{
+      src: 'http://lorempixel.com/500/500/nature',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/abstract',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/sports',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/city',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/food',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/animals',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/business',
+      w: 500, h: 500
+    }, {
+      src: 'http://lorempixel.com/500/500/fashion',
+      w: 500, h: 500
+    }];
+
+  vm.showGallery = function (i) {
+    vm.opts.index = i || vm.opts.index;
+    $scope.$broadcast(vm.startEvent);
+  };
+}
 
 angular.element(document).ready(function() {
   angular.bootstrap(document, ['demoApp']);
